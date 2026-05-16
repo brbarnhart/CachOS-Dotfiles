11e1797749943b17305600572c9a8bd1d71dc894
Ben <brbarnhart99@gmail.com>
2026-05-16 15:17:05 -0500

    initial commit

---
+++ .config/fish/config.fish
@@ -0,0 +1,22 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+# overwrite greeting
+# potentially disabling fastfetch
+#function fish_greeting
+#    # smth smth
+#end
+
+function y
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	command yazi $argv --cwd-file="$tmp"
+	if read -z cwd < "$tmp"; and [ "$cwd" != "$PWD" ]; and test -d "$cwd"
+		builtin cd -- "$cwd"
+	end
+	rm -f -- "$tmp"
+end
+
+set -gx EDITOR "zed --wait"
+set -gx VISUAL "zed --wait"
+
+# uv
+fish_add_path "/home/ben/.local/bin"
