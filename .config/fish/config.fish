9d8b6cdb125f266d656a6ae9648397d92a8d1f6e
Ben <brbarnhart99@gmail.com>
2026-06-25 09:39:25 -0500

    fixed some errors in config files

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,7 +3,7 @@ source /usr/share/cachyos-fish-config/cachyos-config.fish
 # overwrite greeting
 # potentially disabling fastfetch
 function fish_greeting
-    # smth smth
+     #"hello"
 end
 
 function y
